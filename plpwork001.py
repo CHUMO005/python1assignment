13adc2f89d08fa708de76c949384245d35f4e103
Vicky Chumo <148944899+CHUMO005@users.noreply.github.com>
2025-03-23 12:31:03 +0300

    Add files via upload

---
+++ plpwork001.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+num1=float(input("Enter the first number:"))
+num2=float(input("Enter the second number:"))
+operation=input("Enter a operation(+,*,/):")
+if operation=="+":
+    result=num1+num2
+    print(num1,"+",num2,"=",result)
+    elif operation== "*" :
+    result=num1*num2
+    print(num1,"*",num2,"=",result)
+    elif operation=="/":
+    result=num1/num2
+    print(num1,"/",num2,"=",result)
+    else:
+        print("Invalid")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
