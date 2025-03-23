56c3c00eddddf2a49dda05d7e11954181a3f3dbf
Vicky Chumo <148944899+CHUMO005@users.noreply.github.com>
2025-03-23 12:36:19 +0300

    Update plpwork001.py

--- plpwork001.py
+++ plpwork001.py
@@ -8,15 +8,15 @@ num1=float(input("Enter the first number:"))
 num2=float(input("Enter the second number:"))
 operation=input("Enter a operation(+,*,/):")
 if operation=="+":
-    result=num1+num2
-    print(num1,"+",num2,"=",result)
-    elif operation== "*" :
+     result=num1+num2
+     print(num1,"+",num2,"=",result)
+elif operation== "*" :
     result=num1*num2
     print(num1,"*",num2,"=",result)
-    elif operation=="/":
+elif operation=="/":
     result=num1/num2
     print(num1,"/",num2,"=",result)
-    else:
+else:
         print("Invalid")
 
 
